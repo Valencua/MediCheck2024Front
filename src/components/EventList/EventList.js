6c1dfef97675bca32bb40e1001285dbc9308494a
Valencua <110689742+Valencua@users.noreply.github.com>
2024-07-16 23:30:46 -0300

    actualización de estilo y fix de bugs

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -8,13 +8,8 @@ import styles from './EventList.module.css';
 import './EventList.css';
 
 const EventList = ({ day, month, year }) => {
-    const router = useRouter();
-
-    const goBack = () => {
-        router.back();
-    };
-
-    const items = [
+    const [isEditionActive, setIsEditionActive] = useState(false);
+    const [items, setItems] = useState([
         {type: 'Vacunación', time: '8:30 am'},
         {type: 'Medicación', amount: '2 ml', time: '9:00 am'},
         {type: 'Vacunación', time: '10:30 am'},
@@ -24,7 +19,25 @@ const EventList = ({ day, month, year }) => {
             time: '9:00 am',
             details: 'Tomar con un vaso de agua, no con algun jugo y tener en cuenta que despues de la primera dosis provoca cansancio. Ademas recordar que lalalala...'
         }
-    ];
+    ]);
+    const router = useRouter();
+
+    const goBack = () => {
+        router.back();
+    };
+
+    const toggleEdition = () => {
+        setIsEditionActive(!isEditionActive);
+    };
+
+    const removeItem = (itemToRemove) => {
+        if (!isEditionActive){
+            return;
+        }
+        setItems(prevItems => prevItems.filter(item =>
+            !(item.type === itemToRemove.type && item.time === itemToRemove.time && item.details === itemToRemove.details)
+        ));
+    }
 
     return (
         <div className={styles.accordionContainer}>
@@ -32,30 +45,35 @@ const EventList = ({ day, month, year }) => {
                 <div className={styles.flechaBack}  onClick={goBack}></div>
                 <div className={styles.accordionPageHeaderDate}>{day} de {month} {year}</div>
             </div>
-            {items.map((value, index) => (
-                <Accordion key={index} className={styles.accordionListContainer}>
-                    <AccordionSummary
-                        aria-controls={`panel${index}-content`}
-                        id={`panel${index}-header`}
-                    >
-                        <div className={styles.accordionHeader}>
-                            <div
-                                className={value.type === 'Vacunación' ? styles.iconoVacunacion : styles.iconoPildora}></div>
-                            <div className={styles.accordionType}>{value.type}</div>
-                            <div className={styles.accordionAmount}>{value.amount}</div>
-                            <div className={styles.accordionTime}>{value.time}</div>
-                        </div>
-                    </AccordionSummary>
-                    {value.details ?
-                        (
-                            <AccordionDetails>
-                                <div className={styles.accordionDetails}>{value.details}</div>
-                            </AccordionDetails>
-                        ) : null
-                    }
+            {items.map((value, index) => {
+                let iconoEvento = value.type === 'Vacunación' ? styles.iconoVacunacion : styles.iconoPildora;
+                let iconoFila = isEditionActive ? styles.iconoEliminar : iconoEvento;
+                return (
+                    <Accordion key={index} className={styles.accordionListContainer}>
+                        <AccordionSummary
+                            aria-controls={`panel${index}-content`}
+                            id={`panel${index}-header`}
+                        >
+                            <div className={styles.accordionHeader}>
+                                <div className={iconoFila} onClick={() => removeItem(value)}></div>
+                                <div className={styles.accordionType}>{value.type}</div>
+                                <div className={styles.accordionAmount}>{value.amount}</div>
+                                <div className={styles.accordionTime}>{value.time}</div>
+                            </div>
+                        </AccordionSummary>
+                        {value.details ?
+                            (
+                                <AccordionDetails>
+                                    <div className={styles.accordionDetails}>{value.details}</div>
+                                </AccordionDetails>
+                            ) : null
+                        }
 
-                </Accordion>
-            ))}
+                    </Accordion>
+                );
+            })}
+
+            <div className={isEditionActive ? styles.botonEdicionInactivo : styles.botonEdicionActivo} onClick={toggleEdition}></div>
         </div>
     );
 };
