7b9bbdf7dc5aa9a686a007a0e6452f7ce9d6adb2
Valencua <110689742+Valencua@users.noreply.github.com>
2024-08-21 23:00:03 -0300

    LoginIniciado

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,49 @@
 import React, {useState} from 'react';
 import styles from './Login.module.css';
+import './Login.css';
 import {useRouter} from "next/router";
 import {Modal, TextField, Button, IconButton, Box, Typography, Icon} from '@mui/material';
 
 const Login = () => {
 
     const router = useRouter();
+    const [name, setName] = useState('');
+    const [pass, setPass] = useState('');
 
     return (
         <Box className={styles.loginContainer}> 
-            <Box>
-                <Typography variant="h6" className={styles.modalTitle}>Login</Typography>
+            <Box className={styles.containerWithBordersTitle}>
+                <Typography variant="h2" className={styles.modalTitle}>LOGIN</Typography>
             </Box>
-            <Box className={styles.containerWithBorders}>
+            <Box className={styles.containerWithBordersLogo}>
                     <div className={styles.icon}></div>
+                    <div className={styles.iconTitle}></div>
+            </Box>
+            <Box className={styles.containerWithBordersName}>
+                <Typography variant="h6" className={styles.labelUser}>Nombre Completo</Typography>
+                <TextField
+                        className={styles.nameInput}
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        fullWidth
+                        margin="normal"
+                />
+            </Box>
+            <Box className={styles.containerWithBordersPassword}>
+                <Typography variant="h6" className={styles.labelUser}>Contraseña</Typography>
+                <TextField
+                        className={styles.nameInput}
+                        onChange={(e) => setPass(e.target.value)}
+                        value={pass}
+                        fullWidth
+                        margin="normal"
+                />
+            </Box>
+            <Box className={styles.buttonContainer}>
+                    <Button className={styles.buttonModal} variant="contained" color="primary" fullWidth>
+                        Confirmar
+                    </Button>
+                    <Typography variant="h6" className={styles.labelUser}>¿No tienes cuenta?</Typography>
             </Box>
         </Box>
     );
